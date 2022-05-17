0cf164f2443cbe9a032694ba405034dfdaadc0c4
Paulotx <pauloandre0011@gmail.com>
2022-05-17 20:58:20 -0300

    Register singleton users token

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,9 +3,16 @@ import { container } from 'tsyringe';
 import './providers';
 
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { IUserTokensRepository } from '@modules/users/repositories/IUserTokensRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );
