cf1801105df92a1094574ee51e8dc79c79010025
Paulotx <pauloandre0011@gmail.com>
2022-05-17 01:44:03 -0300

    Create user entitie

---
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Entity,
+  PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  profile: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
