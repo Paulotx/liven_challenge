c6aeed4246a544fa36c6b117d5fb9807ba2fec29
Paulotx <pauloandre0011@gmail.com>
2022-05-18 18:35:19 -0300

    Add list users router

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
+import { ListUsersController } from '@modules/users/useCases/listUsers/ListUsersController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const listUsersController = new ListUsersController();
 
 usersRoutes.use(ensureAuthenticated);
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.get('/', listUsersController.handle);
 
 export default usersRoutes;
