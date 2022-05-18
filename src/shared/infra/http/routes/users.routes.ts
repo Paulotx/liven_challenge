4c66faa1359a99ad48495dc597a19503d497f9f1
Paulotx <pauloandre0011@gmail.com>
2022-05-18 18:36:42 -0300

    Add show user router

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
 import { ListUsersController } from '@modules/users/useCases/listUsers/ListUsersController';
+import { ShowUserController } from '@modules/users/useCases/showUser/ShowUserController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -9,10 +10,12 @@ const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
+const showUserController = new ShowUserController();
 
 usersRoutes.use(ensureAuthenticated);
 
 usersRoutes.post('/', createUserController.handle);
 usersRoutes.get('/', listUsersController.handle);
+usersRoutes.get('/:id', showUserController.handle);
 
 export default usersRoutes;
