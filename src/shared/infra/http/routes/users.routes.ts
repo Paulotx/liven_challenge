3731ed61871736ae08e21716803f379a64b49926
Paulotx <pauloandre0011@gmail.com>
2022-05-18 18:41:18 -0300

    Create delete user

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
+import { DeleteUserController } from '@modules/users/useCases/deleteUser/DeleteUserController';
 import { ListUsersController } from '@modules/users/useCases/listUsers/ListUsersController';
 import { ShowUserController } from '@modules/users/useCases/showUser/ShowUserController';
 import { UpdateUserController } from '@modules/users/useCases/updateUser/UpdateUserController';
@@ -13,6 +14,7 @@ const createUserController = new CreateUserController();
 const listUsersController = new ListUsersController();
 const showUserController = new ShowUserController();
 const updateUserController = new UpdateUserController();
+const deleteUserController = new DeleteUserController();
 
 usersRoutes.use(ensureAuthenticated);
 
@@ -20,5 +22,6 @@ usersRoutes.post('/', createUserController.handle);
 usersRoutes.get('/', listUsersController.handle);
 usersRoutes.get('/:id', showUserController.handle);
 usersRoutes.put('/:id', updateUserController.handle);
+usersRoutes.delete('/:id', deleteUserController.handle);
 
 export default usersRoutes;
