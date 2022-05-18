88c78169657526addd712a706e9629f4b8ffff98
Paulotx <pauloandre0011@gmail.com>
2022-05-17 21:22:45 -0300

    Create ensure authenticated

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,9 +2,13 @@ import { Router } from 'express';
 
 import { CreateUserController } from '@modules/users/useCases/createUser/CreateUserController';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+
+usersRoutes.use(ensureAuthenticated);
 
 usersRoutes.post('/', createUserController.handle);
 
