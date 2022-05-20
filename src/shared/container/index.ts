9f8b00e26bac56a2f7617e0784246d47ad3010f6
Paulotx <pauloandre0011@gmail.com>
2022-05-19 22:18:53 -0300

    Add register singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,8 @@ import { container } from 'tsyringe';
 
 import './providers';
 
+import { AddressesRepository } from '@modules/addresses/infra/typeorm/repositories/AddressesRepository';
+import { IAddressesRepository } from '@modules/addresses/repositories/IAddressesRepository';
 import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
@@ -15,4 +17,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+
+container.registerSingleton<IAddressesRepository>(
+  'AddressesRepository',
+  AddressesRepository,
 );
