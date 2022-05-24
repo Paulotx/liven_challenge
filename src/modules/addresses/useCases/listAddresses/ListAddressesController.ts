b603ffb41c2554a60fe5963827de82812ae683da
Paulotx <pauloandre0011@gmail.com>
2022-05-23 23:24:07 -0300

    Add query params

--- src/modules/addresses/useCases/listAddresses/ListAddressesController.ts
+++ src/modules/addresses/useCases/listAddresses/ListAddressesController.ts
@@ -5,9 +5,14 @@ import { ListAddressesUseCase } from './ListAddressesUseCase';
 
 export class ListAddressesController {
   async handle(request: Request, response: Response): Promise<Response> {
+    const { country } = request.query;
+    console.log(country);
+
     const listAddressesUseCase = container.resolve(ListAddressesUseCase);
 
-    const addresses = await listAddressesUseCase.execute();
+    const addresses = await listAddressesUseCase.execute({
+      country: country ? String(country) : undefined,
+    });
 
     return response.status(200).json(addresses);
   }
--- src/modules/addresses/useCases/listAddresses/ListAddressesUseCase.ts
+++ src/modules/addresses/useCases/listAddresses/ListAddressesUseCase.ts
@@ -3,6 +3,10 @@ import { inject, injectable } from 'tsyringe';
 import { Address } from '@modules/addresses/infra/typeorm/entities/Address';
 import { IAddressesRepository } from '@modules/addresses/repositories/IAddressesRepository';
 
+interface IRequest {
+  country: string | undefined;
+}
+
 @injectable()
 export class ListAddressesUseCase {
   constructor(
@@ -10,8 +14,16 @@ export class ListAddressesUseCase {
     private addressesRepository: IAddressesRepository,
   ) {}
 
-  async execute(): Promise<Address[]> {
-    const addresses = this.addressesRepository.listAll();
-    return addresses;
+  async execute({ country }: IRequest): Promise<Address[]> {
+    const addresses = await this.addressesRepository.listAll();
+
+    return addresses.filter((address) => {
+      if (country) {
+        return (
+          address.country.toLocaleLowerCase() === country.toLocaleLowerCase()
+        );
+      }
+      return true;
+    });
   }
 }
